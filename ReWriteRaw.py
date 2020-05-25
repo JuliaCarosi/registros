972db6617eb3553894a4349b9d31b52bd5135f99
Julia Carosi <jcarosi@itba.edu.ar>
2020-05-25 15:16:35 -0300

    add grid 0.5

--- ReWriteRaw.py
+++ ReWriteRaw.py
@@ -41,29 +41,40 @@ plt.show(block=False)
 print('Sampling Frequency: %d' %  raw.info['sfreq'] )
 
 #pplot = raw.plot(scalings='auto',n_channels=10,block=True, )
-
+from scipy import signal
+import matplotlib.pyplot as plt
+t = np.linspace(0, 3318, 663804, endpoint=False)    #me creo mi señal con pulso de 0.5 seg
+plt.plot(t, signal.square(2 * np.pi * 1 * t))
+plt.ylim(-2, 2)
 
 # (2) Con este código extraigo los datos que necesito y me rearmo la estructura que necesito para poder analizarlo mejor
 data =raw.get_data()                            # Saco los datos concretos, una matriz de numpy
+print(data[0:5])
 canal_eogs = data[6,:] - data[7,:]                   # Cree la variable de la resta de las dos señales
 canal_emgs =  data[8,:] - data[9,:]
 data[6]=canal_eogs
 data[7]=canal_emgs
-data=data[[0,1,2,3,4,5,6,7], :]
-new_ch_names =[ raw.ch_names[0], raw.ch_names[1],raw.ch_names[2] , raw.ch_names[3],  raw.ch_names[4],  raw.ch_names[5], "EOG_resta", "EMG_resta"] 
+data[8]=signal.square(2 * np.pi * 1 * t)
+data=data[[0,1,2,3,4,5,6,7,8], :]
+new_ch_names =[ raw.ch_names[0], raw.ch_names[1],raw.ch_names[2] , raw.ch_names[3],  raw.ch_names[4],  raw.ch_names[5], "EOG_resta", "EMG_resta", "Pulso"] 
+
 
 ch_names = ['Supera75'] + new_ch_names              # Saco el nombre de los canales pero agrego uno 'peak'
 sfreq = raw.info['sfreq']
 
+
 dat = np.concatenate( (np.zeros((1,data.shape[1])), data), axis=0)    # Le agrego a los datos un array con zeros.
-                                                                     # aca si ustedes quieren pueden agregarle 20 o algo asi
+
+                                                                # aca si ustedes quieren pueden agregarle 20 o algo asi
                                                                       # cada vez que la señal que ustedes analizan supera los 75
+
+
 
 
 dat = np.concatenate( (np.zeros ((1, canal_eogs.shape[0])), data), axis=0) 
 dat = np.concatenate( (np.zeros ((1, canal_emgs.shape[0])), data), axis=0) 
 
-ch_types = ['misc'] + ['eeg' for _ in ch_names[0:6]] + ['eog','emg']   # Recompongo los canales.
+ch_types = ['misc'] + ['eeg' for _ in ch_names[0:6]] + ['misc','misc'] + ['misc']  # Recompongo los canales.
 info = mne.create_info(ch_names, sfreq, ch_types=ch_types)
 
 
@@ -73,8 +84,11 @@ info['meas_date'] = raw.info['meas_date']       # Registro el timestamp para las
 reraw = mne.io.RawArray(dat, info)
 reraw.set_annotations(raw.annotations)          # Construyo un nuevo objeto raw que tiene lo que necesito.
 
-reraw.plot(scalings='auto',n_channels=10,block=True, )
-pplot=reraw.plot(scalings='auto', n_channels=10, block=True, )
+#reraw.plot(scalings='auto',n_channels=10,block=True, )
+#pplot=reraw.plot(scalings='auto', n_channels=10, block=True, )
 
+reraw_copy=reraw.copy()
+reraw_copy.drop_channels(['F3_1','F4_1','P3_1','P4_1'])
+pplot=reraw_copy.plot(scalings='auto', duration=30, n_channels=10, block=True, )
 
 # POR Ejemplo con esto pueden restar EMG1 y EMG2 y dejar solo uno, lo mismo con EOG.
