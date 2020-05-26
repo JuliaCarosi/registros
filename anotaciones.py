c2a9a524de7cdcb733348cc59ed7fbf79490abc0
giulicarbonari <50453356+giulicarbonari@users.noreply.github.com>
2020-05-26 20:48:05 -0300

    correciones en los pedidos de datos al usuario

--- anotaciones.py
+++ anotaciones.py
@@ -34,10 +34,10 @@ def set_sleep_states(raw,path):
     description = np.zeros((cant_anotations))  #array descripcion de la etiqueta
     start = 0
     for i in range(cant_anotations):
-        start= start + 30 #* sfreq
         onset[i] = start
         duration[i] = 30 #* sfreq
         description[i] = estados[i]
+        start= start + 30 #* sfreq
 
     states_anotations = mne.Annotations(onset,duration,description)    #<Annotations | 136 segments: 0.0 (50), 1.0 (20), 2.0 (42), 3.0 (24)>
     raw2 = raw.copy().set_annotations(states_anotations)
@@ -49,72 +49,84 @@ def extract_signal(raw):
         if (element.description == 'K'):
             print('onset: ', element.get('onset'), 'duration: ', element.get('duration'))
 
+def new_raw_data(raw,sfreq):
+    data =raw.get_data()                                 # Saco los datos concretos, una matriz de numpy
+    time_shape = data.shape[1]
+    
+    # Con este código extraigo los datos que necesito y me rearmo la estructura que necesito para poder analizarlo mejor
+    eog = (raw.copy()).pick_types(eog=True)
+    eog_data = eog.get_data()
+    sub_eog = eog_data[0,:]-eog_data[1,:]
+    emg = (raw.copy()).pick_types(misc=True)
+    emg_data = emg.get_data()
+    sub_emg = emg_data[0,:]-emg_data[1,:]
+    t = np.linspace(1, round(time_shape/sfreq), time_shape, endpoint=False)    #me creo mi señal artificial con pulso de 0.5 seg
+    pulso = signal.square(2 * np.pi * 1 * t) #señal del pulso
+    pos = (raw.ch_names).index('C3_1')
+    c3_1 = data[pos,:]
+    pos2 =(raw.ch_names).index('C4_1')
+    c4_1 = data[pos2,:]
+    new_data=data.copy()
+    new_data[0]= sub_eog
+    new_data[1]= sub_emg
+    new_data[2]= pulso
+    new_data[3]= c3_1
+    new_data[4]= c4_1
+    new_data=new_data[[0,1,2,3,4], :]
+    new_ch_names = ['EOG', 'EMG', 'Pulse', 'C3', 'C4']
+    new_chtypes = 3* ['misc'] + 2 *['eeg'] # Recompongo los canales.
+    new_info = mne.create_info(new_ch_names, sfreq, ch_types=new_chtypes)
+    new_info['meas_date'] = raw.info['meas_date']       # Registro el timestamp para las anotaciones.
+    new_raw=mne.io.RawArray(new_data, new_info)
+    new_raw.set_annotations(raw.annotations)           # Construyo un nuevo objeto raw que tiene lo que necesito.
+    return(new_raw)
 
 def main():
     scal = dict(mag=1e-12, grad=4e-11, eeg=20e-5, eog=150e-6, ecg=5e-4,
             emg=1e-4, ref_meg=1e-12, misc=1e-3, stim=1,
             resp=1, chpi=1e-4, whitened=1e2)
 
-    messagebox.showinfo(message="Selecciona el archivo vhdr", title="Seleccion de datos")
-    path = easygui.fileopenbox(title='Seleccione vhdr')#selecciono la carpeta vhdr
-    subject = get_name(path)
-    
-    mne.set_log_level("WARNING")
-    raw= mne.io.read_raw_brainvision(path, 
-        preload=True, 
-        eog=('EOG1_1','EOG2_1'),
-        misc=('EMG1_1','EMG2_1'),
-        verbose=True)
-    raw.rename_channels(lambda s: s.strip("."))
-
-    info = raw.info
-    sfreq = info.get('sfreq') #frecuencia de muestreo
-    
     anotaciones = messagebox.askquestion(message="El archivo ya posee anotaciones?", title="Anotaciones")
-    if (anotaciones == 'yes'):
+    if (anotaciones == 'no'):
+        messagebox.showinfo(message="Selecciona el archivo vhdr", title="Seleccion de datos")
+        path = easygui.fileopenbox(title='Seleccione vhdr')#selecciono la carpeta vhdr
+
+        mne.set_log_level("WARNING")
+        raw= mne.io.read_raw_brainvision(path, 
+            preload=True, 
+            eog=('EOG1_1','EOG2_1'),
+            misc=('EMG1_1','EMG2_1'),
+            verbose=True)
+        raw.rename_channels(lambda s: s.strip("."))
+    
         messagebox.showinfo(message="Selecciona txt con etapas de sueño", title="Seleccion de Etapas de sueño")
-        path_annotations = easygui.fileopenbox(title='Seleccione txt con anotaciones') #selecciono el txt de anotaciones anteriores
-        raw.set_annotations(mne.read_annotations(path_annotations))
-    elif(anotaciones == 'no'):
-        messagebox.showinfo(message="Selecciona el txt con anotaciones", title="Seleccion de anotaciones")
-        path_states = easygui.fileopenbox(title='Seleccione txt con etapa de sueño') #selecciono el txt de estados de sueño
+        path_states = easygui.fileopenbox(title='Seleccione txt con anotaciones') #selecciono el txt de anotaciones anteriores
         raw = set_sleep_states(raw,path_states)
 
-    # -----------------------------------------------------------------
-    data =raw.get_data()         # data(chan,samp), times(1xsamples)
-    info = raw.info              #info
-    sfreq = info.get('sfreq')    #frecuencia de muestreo
+        info = raw.info
+        sfreq = info.get('sfreq') #frecuencia de muestreo
+        raw = new_raw_data(raw,sfreq)
 
-    #Con este código extraigo los datos que necesito y me rearmo la estructura que necesito para poder analizarlo mejor
-    data =raw.get_data()                                 # Saco los datos concretos, una matriz de numpy
-    new_data=data.copy()
-    canal_eogs = data[6,:] - data[7,:]                   # Cree la variable de la resta de las dos señales
-    canal_emgs =  data[8,:] - data[9,:]
-    t = np.linspace(1, round(data.shape[1]/sfreq), data.shape[1], endpoint=False)   
-    new_data[0]= signal.square(2 * np.pi * 1 * t)
-    new_data[1]=data[[0], :]
-    new_data[2]=data[[1], :]
-    new_data[3]=canal_eogs
-    new_data[4]=canal_emgs
+    elif(anotaciones == 'yes'): #evita pasar por todo lo mismo de nuevo 
+        messagebox.showinfo(message="Selecciona el archivo fif", title="Seleccion de datos")
+        path = easygui.fileopenbox(title='Seleccione fif')#selecciono la carpeta vhdr
+        raw = mne.io.read_raw_fif(path)
 
-    new_data=new_data[[0,1,2,3,4], :]        #Elimino los otros canales
+        messagebox.showinfo(message="Selecciona el txt con anotaciones", title="Seleccion de anotaciones")
+        path_annotations = easygui.fileopenbox(title='Seleccione txt con etapa de sueño') #selecciono el txt de estados de sueño
+        raw.set_annotations(mne.read_annotations(path_annotations))
 
-    new_chnames =[ "Pulso", raw.ch_names[0], raw.ch_names[1], "EOG_resta", "EMG_resta"] 
-    new_chtypes = ['misc'] +['eeg' for _ in new_chnames[0:2]] + ['misc','misc'] # Recompongo los canales.
+  
+    subject = get_name(path)
+    info = raw.info
+    sfreq = info.get('sfreq') #frecuencia de muestre
+    
+    raw.plot(show_options=True,title='Etiquetado',start=0,duration=30,n_channels=10, scalings=scal,block=True,order=[0,3,2,4,1])
+    #order cambia el orden en el que aparecen los canales
 
-    new_info = mne.create_info(new_chnames, sfreq, ch_types=new_chtypes)
-    new_info['meas_date'] = raw.info['meas_date']       # Registro el timestamp para las anotaciones.
+    raw.annotations.save(subject + "Annotations.txt")
 
-    new_raw=mne.io.RawArray(new_data, new_info)
-    new_raw.set_annotations(raw.annotations)           # Construyo un nuevo objeto raw que tiene lo que necesito.
-
-    scal = dict(mag=1e-12, grad=4e-11, eeg=20e-5, eog=150e-6, ecg=5e-4,emg=1e-4, ref_meg=1e-12, misc=1e-3, stim=1,
-        resp=1, chpi=1e-4, whitened=1e2)
-
-    #pplot=new_raw.plot(scalings=scal, duration=30, n_channels=10, block=True, )
-        
-    new_raw.plot(show_options=True,title='Etiquetado',start=0,duration=30,n_channels=10, scalings=scal,block=True)
-    new_raw.annotations.save(subject + "Annotations.txt")
+    raw.save(subject + ".fif")
 
 
 
